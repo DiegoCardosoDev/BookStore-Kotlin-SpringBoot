62e2c6ffe357406848984caf8d660406ca5f4ca6
dcardososo <diego.sacramento@engdb.com.br>
2022-05-19 20:20:38 -0300

    refatory model

--- salebooks/src/main/kotlin/br/com/diego/salebooks/models/CustomerModel.kt
+++ salebooks/src/main/kotlin/br/com/diego/salebooks/models/CustomerModel.kt
@@ -1,5 +1,6 @@
 package br.com.diego.salebooks.models
 
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -8,9 +9,16 @@ import javax.persistence.Id
 @Entity
 data class CustomerModel(
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Int,
+    var id: Int? = null,
+
+    @Column
     var name: String,
+
+    @Column
     var email: String
+
+
 )
