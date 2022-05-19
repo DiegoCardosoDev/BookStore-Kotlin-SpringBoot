07ffa3ea9dd7b89cc55dfc9df15cff76abd5ed7b
dcardososo <diego.sacramento@engdb.com.br>
2022-05-19 20:22:55 -0300

    add flyway dependencies

--- salebooks/build.gradle.kts
+++ salebooks/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.flywaydb:flyway-core:8.5.11")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
