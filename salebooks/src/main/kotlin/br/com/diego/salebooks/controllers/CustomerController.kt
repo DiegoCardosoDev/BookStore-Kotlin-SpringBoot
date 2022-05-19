10891e10bc7d72b0a0562568621d5f7d17d6f20f
dcardososo <diego.sacramento@engdb.com.br>
2022-05-19 20:21:29 -0300

    refatory controller inject dependencies

--- salebooks/src/main/kotlin/br/com/diego/salebooks/controllers/CustomerController.kt
+++ salebooks/src/main/kotlin/br/com/diego/salebooks/controllers/CustomerController.kt
@@ -1,29 +1,48 @@
 package br.com.diego.salebooks.controllers
 
 import br.com.diego.salebooks.controllers.request.PostCustomerRequest
+import br.com.diego.salebooks.controllers.request.PutCustomerRequest
+import br.com.diego.salebooks.extension.toCustomerModel
 import br.com.diego.salebooks.models.CustomerModel
-import br.com.diego.salebooks.repository.CustomerRepository
+import br.com.diego.salebooks.service.CustomerService
+import lombok.extern.slf4j.Slf4j
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
-
+@Slf4j
 @RestController
-@RequestMapping("custumer")
-class CustomerController(val repository: CustomerRepository) {
+@RequestMapping("customer")
+class CustomerController(
+    val customerService: CustomerService
+) {
 
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    fun getCustomer(): MutableIterable<CustomerModel> {
-        return repository.findAll()
-
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+        return customerService.getAll(name)
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createCustomer(@RequestBody customerModel: PostCustomerRequest) {
-        println(customerModel)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        customerService.create(customer.toCustomerModel())
+    }
 
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: Int): CustomerModel {
+        return customerService.getCustomer(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody customer: PutCustomerRequest) {
+        customerService.update(customer.toCustomerModel(id))
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) {
+        customerService.delete(id)
     }
 
 
