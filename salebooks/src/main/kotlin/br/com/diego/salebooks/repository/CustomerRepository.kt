0a92e58e545d24bcc3894a0a2591940236818943
dcardososo <diego.sacramento@engdb.com.br>
2022-05-19 20:20:16 -0300

    refatory repo

--- salebooks/src/main/kotlin/br/com/diego/salebooks/repository/CustomerRepository.kt
+++ salebooks/src/main/kotlin/br/com/diego/salebooks/repository/CustomerRepository.kt
@@ -3,4 +3,7 @@ package br.com.diego.salebooks.repository
 import br.com.diego.salebooks.models.CustomerModel
 import org.springframework.data.repository.CrudRepository
 
-interface CustomerRepository : CrudRepository<CustomerModel, Long>
+interface CustomerRepository : CrudRepository<CustomerModel, Int>{
+
+    fun findByNameContainingIgnoreCase(name: String): List<CustomerModel>
+}
