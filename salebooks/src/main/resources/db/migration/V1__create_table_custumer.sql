fe498d18677ccfc13317ebc83eb6ae136dfc8b9d
dcardososo <diego.sacramento@engdb.com.br>
2022-05-19 22:25:01 -0300

    V2_sql insert

--- salebooks/src/main/resources/db/migration/V1__create_table_custumer.sql
+++ salebooks/src/main/resources/db/migration/V1__create_table_custumer.sql
@@ -1,7 +1,7 @@
 CREATE TABLE customer(
 
- id int auto_increment primary key,
- name varchar(255) not null,
- email varchar(255) not null unique
+      id int auto_increment primary key,
+      name varchar(255) not null,
+      email varchar(255) not null unique
 
 );
