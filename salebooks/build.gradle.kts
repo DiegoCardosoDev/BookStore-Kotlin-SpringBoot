e70dac7174f51011f52295efe14f8ef3d015fa11
DiegoCardoso <diegocardososacramento@gmail.com>
2022-09-14 20:47:57 -0300

    feat:testes para customer service method FindAll

--- salebooks/build.gradle.kts
+++ salebooks/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.12.0")
+
 
 	runtimeOnly("mysql:mysql-connector-java")
 	implementation("org.flywaydb:flyway-core:8.5.10")
@@ -45,6 +47,7 @@ dependencies {
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
+
 
 
 
---
+++ salebooks/src/test/kotlin/br/com/diego/salebooks/service/CustomerServiceTest.kt
@@ -0,0 +1,54 @@
+package br.com.diego.salebooks.service
+import br.com.diego.salebooks.enums.CustomerStatus
+import br.com.diego.salebooks.enums.Role
+import br.com.diego.salebooks.models.CustomerModel
+import br.com.diego.salebooks.repository.CustomerRepository
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import java.util.UUID
+
+@ExtendWith(MockKExtension::class)
+ class CustomerServiceTest{
+
+ @MockK
+ private lateinit var customerRepository: CustomerRepository
+ @MockK
+ private lateinit var bookService: BookService
+ @MockK
+ private lateinit var bCryptPasswordEncoder: BCryptPasswordEncoder
+
+  @InjectMockKs
+  private lateinit var customerService: CustomerService
+
+ @Test
+ fun `should return all customers`(){
+
+  val fakeCustomers= listOf(buildCustomer(), buildCustomer())
+  every { customerRepository.findAll() } returns fakeCustomers
+  val customers = customerService.getAll(null)
+  assertEquals(fakeCustomers, customers)
+  verify(exactly = 1) { customerRepository.findAll() }
+  verify(exactly = 0) { customerRepository.findByNameContainingIgnoreCase(any()) }
+
+ }
+ fun buildCustomer(id:Int?=null,
+                   name:String="customerName",
+                   email:String = "${UUID.randomUUID()}@email.com",
+                   password:String="pass"
+ )=CustomerModel(
+         id=id,
+         name=name,
+         email=email,
+         status = CustomerStatus.ATIVO,
+         password=password,
+         roles = setOf(Role.CUSTOMER)
+ )
+
+ }
