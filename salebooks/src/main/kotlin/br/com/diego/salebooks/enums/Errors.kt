3363a839b4566230a6001b821c260a66179d744a
DiegoCardoso <diegocardososacramento@gmail.com>
2022-09-19 19:57:22 -0300

    mudei os códigos de erro

--- salebooks/src/main/kotlin/br/com/diego/salebooks/enums/Errors.kt
+++ salebooks/src/main/kotlin/br/com/diego/salebooks/enums/Errors.kt
@@ -1,10 +1,11 @@
 package br.com.diego.salebooks.enums
 
 enum class Errors(val code: String, val message: String) {
-    BK000("BK-000", "unauthorized"),
-    BK101("BK-101", "[%s] não existe"),
-    BK102("BK-102", "não é possivel deletra um livro [%s] "),
-    BK201("BK-201", "[%s]"),
-    BK001("BK-001", "request inválido[%s]")
+    ML000("ML-000", "Access Denied"),
+    ML001("ML-001", "Invalid Request"),
+    ML101("ML-101", "Book [%s] not exists"),
+    ML102("ML-102", "Cannot update book with status [%s]"),
+    ML201("ML-201", "Customer [%s] not exists")
+
 
 }
